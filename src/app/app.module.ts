2a035107e80966121d97b370dea467243e7d64be
Alexey Karpov <karpovad@yandex.ru>
2018-07-01 16:52:40 +0200

    Apollo setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule, Injector } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
+import { Injector, NgModule } from '@angular/core'
 import { createCustomElement } from '@angular/elements'
-
+import { BrowserModule } from '@angular/platform-browser'
+import { Apollo, ApolloModule } from 'apollo-angular'
+import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import { AppComponent } from './app.component'
 import { DailyActiveAdressesComponent } from './daily-active-adresses/daily-active-adresses.component'
 
 @NgModule({
   declarations: [AppComponent, DailyActiveAdressesComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule, ApolloModule, HttpLinkModule],
   providers: [],
   entryComponents: [DailyActiveAdressesComponent],
 })
 export class AppModule {
-  constructor(private injector: Injector) {}
+  constructor(private injector: Injector, apollo: Apollo, httpLink: HttpLink) {
+    apollo.create({
+      link: httpLink.create({
+        uri: 'https://sanbase-low.santiment.net/graphql',
+      }),
+      cache: new InMemoryCache(),
+    })
+  }
 
   ngDoBootstrap() {
     const el = createCustomElement(DailyActiveAdressesComponent, {
