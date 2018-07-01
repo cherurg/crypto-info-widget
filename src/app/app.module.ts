36e11dba1b0bddcf026745ee169edfa6a8f9229b
Alexey Karpov <karpovad@yandex.ru>
2018-07-01 15:41:01 +0200

    User-poll component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule, Injector } from '@angular/core'
+import { createCustomElement } from '@angular/elements'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { UserPollComponent } from './user-poll/user-poll.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, UserPollComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [UserPollComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const el = createCustomElement(UserPollComponent, {
+      injector: this.injector,
+    })
+
+    customElements.define('user-poll', el)
+  }
+}
